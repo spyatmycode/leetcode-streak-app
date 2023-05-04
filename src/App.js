186079cd0067c63c29049cef3f992a5017b7b5ff
spyatmycode <akejunifemi11@gmail.com>
2023-05-04 12:00:13 +0100

    feat(assets folder): added new asset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  const getData =async () => {
+    const fetched = await fetch('https://leetcode-stats-api.herokuapp.com/_nifemiakeju_')
+    const data = await fetched.json()
+    setLoading(false)
+    setData(data)
+
+    console.log(await data);
+
+   
+  }
+  useEffect(() => {
+    getData()
+  },[])
+
+
+const {submissionCalendar} = data
+console.log(submissionCalendar);
+
+console.log(new Date(1683072000
+* 1000  ));
+  
+
+  // console.log(submissionCalender);
   return (
     <div>App</div>
   )
